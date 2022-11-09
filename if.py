370bc840625de1b63559094f697fd25138191c6a
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-11-09 12:14:46 +0100

    if

---
+++ if.py
@@ -0,0 +1,5 @@
+d = 'name'
+x = 2j
+
+if type(d) != type(x):
+    print('different')
--- loop.py
+++ loop.py
@@ -15,3 +15,15 @@ for i in mb.keys():
 # items - key|valeur
 for x, y in mb.items():
     print(x, y)
+
+members = {}
+members.update({
+    "name": 'username',
+    'age': 20
+})
+
+turn = 0
+for x, y in members.items():
+    if turn <= 1:
+        turn += 1
+        print(x, y)
