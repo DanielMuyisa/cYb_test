13a1ca98cac04f5edc994341ec22a5f321d11024
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-11-09 13:46:06 +0100

    lamda fx

---
+++ lambda_fx.py
@@ -0,0 +1,18 @@
+# fx lambda
+# x = lambda a : a + 10
+def x(a): return a+10
+
+
+def x(a, b): return a * b
+
+# anonmous fonction lambda
+
+
+def anonymous_lambda(an):
+    return lambda a: a * an
+
+
+newfx = anonymous_lambda(5)
+mlti = newfx(3)
+
+print(mlti)
