39ed24bb83a27a3b70d2d9d2de4898999621fc84
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-11-11 06:20:36 +0100

    pandas_series

--- PANDAS/serie.py
+++ PANDAS/serie.py
@@ -1,5 +1,30 @@
 import pandas as pd
 
-a = [1, 2, 3, 4]
-myvar = pd.Series(a)
-print(myvar)
+a = [1, 2, 3]
+rs = pd.Series(a, index=["x", "y", "z"])
+
+print(rs)
+print(rs['x'])
+
+# key/value
+print("------------")
+net = {
+    "router": 10,
+    "switchs": 20,
+    "topology": 'annau',
+}
+rs = pd.Series(net)
+
+# select
+rs = pd.Series(net, index=["router"])
+print(rs)
+
+# ***** DATA FRAME
+print("------------")
+data = {
+    "one": [1, 2, 3, 4, 5, 6],
+    "two": ['a', 'b', 'c', 'd', 'e', 'f']
+}
+
+rs = pd.Series(data)
+print(rs)
