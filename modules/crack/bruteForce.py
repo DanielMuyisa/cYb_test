6db27272888c3b1d1b0523a4210d5767b2c28809
Daniel Muyisa <danielmuyisa37@gmail.com>
2023-02-04 18:41:32 +0100

    build hash from user input

---
+++ modules/crack/bruteForce.py
@@ -0,0 +1,7 @@
+from brute import brute
+
+# Iterate over *only* numbers (0 - 9).
+# for s in brute(length=5, letters=True, numbers=True, symbols=True):
+#     print(s)
+# print("demo brute force")
+import hashlib
